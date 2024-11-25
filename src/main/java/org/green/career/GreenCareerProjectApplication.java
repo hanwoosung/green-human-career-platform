960aec8ebf610ca85620068644666dd84ca97d4d
unknown <yiok79@naver.com>
2024-11-25 21:06:46 +0900

    깃 셋팅

---
+++ src/main/java/org/green/career/GreenCareerProjectApplication.java
@@ -0,0 +1,13 @@
+package org.green.career;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenCareerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenCareerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/green/career/GreenCareerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.green.career;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenCareerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
