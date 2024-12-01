175a66c4c376e3d4591525f19c8252d156733624
rim <mxmmxqq@gmail.com>
2024-12-01 23:47:47 +0900

    기ì기본 로그인 구현 및 ì공통템플릿 ì 주ì주 적용 및

---
+++ src/main/java/org/green/career/dto/login/UserLoginDto.java
@@ -0,0 +1,15 @@
+package org.green.career.dto.login;
+
+import lombok.Data;
+
+/**
+ * 작성자: 구경림
+ * 작성일: 2024-12-01
+ * UserLogin Dto
+ */
+@Data
+public class UserLoginDto {
+    private String id;
+    private String name;
+    private String userGbnCd;
+}
