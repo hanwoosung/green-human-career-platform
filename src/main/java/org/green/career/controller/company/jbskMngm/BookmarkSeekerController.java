a6362f33443e45e69180d0469810af5c74705db0
K-SangJun <tkdwns000817@gmail.com>
2024-12-05 20:35:26 +0900

    기업 북마크 구직자 조회 -완-

---
+++ src/main/java/org/green/career/controller/company/jbskMngm/BookmarkSeekerController.java
@@ -0,0 +1,42 @@
+package org.green.career.controller.company.jbskMngm;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.green.career.controller.AbstractController;
+import org.green.career.service.company.jbskMngm.bookmarkSeeker.BookmarkSeekerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/company/jbsk-mngm/bookmark-seeker")
+@RequiredArgsConstructor
+@Slf4j
+public class BookmarkSeekerController extends AbstractController {
+
+    final private BookmarkSeekerService bookmarkSeekerService;
+
+    @GetMapping
+    public String jobOffer(Model model,
+                           @RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "search", defaultValue = "") String search) {
+
+        String id = sessionUserInfo("userId");
+
+        Map<String, Object> result = bookmarkSeekerService.getBookmarkSeekerList(page, search, id);
+
+        model.addAttribute("paging", result.get("paging"));
+        model.addAttribute("list", result.get("list"));
+
+        System.out.println(result.get("list"));
+
+        model.addAttribute("search", search);
+
+        return "company/jbskMngm/bookmark_seeker";
+    }
+
+}
---
+++ src/main/java/org/green/career/service/company/jbskMngm/bookmarkSeeker/BookmarkSeekerService.java
@@ -0,0 +1,9 @@
+package org.green.career.service.company.jbskMngm.bookmarkSeeker;
+
+import java.util.Map;
+
+public interface BookmarkSeekerService {
+
+    Map<String, Object> getBookmarkSeekerList(int page, String search, String id);
+
+}
